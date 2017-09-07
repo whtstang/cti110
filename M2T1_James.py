850494958a8ebede9fefe222fa0313f5248d3a7f
whtstang <31742581+whtstang@users.noreply.github.com>
2017-09-07 20:34:11 +0200

    Add files via upload
    Sales Prediction program

---
+++ M2T1_James.py
@@ -0,0 +1,8 @@
+# CTI-110
+# M2T1 - Sales Prediction
+# Jeremiah James
+# 06 Sep 2017
+totalSales = float(input("Projected Total Annual Sales: "))
+annualProfit = .23
+print("Estimated Annnual Profit:", "%.2f"%(totalSales * annualProfit))
+
