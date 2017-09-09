5711cb9945095f2acd1a44454eb634ce585d8cdd
whtstang <31742581+whtstang@users.noreply.github.com>
2017-09-09 19:31:20 +0200

    Delete M2T1_James.py

--- M2T1_James.py
+++
@@ -1,8 +0,0 @@
-# CTI-110
-# M2T1 - Sales Prediction
-# Jeremiah James
-# 06 Sep 2017
-totalSales = float(input("Projected Total Annual Sales: "))
-annualProfit = .23
-print("Estimated Annnual Profit:", "%.2f"%(totalSales * annualProfit))
-
